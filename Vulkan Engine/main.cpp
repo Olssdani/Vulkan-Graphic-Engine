d7bca4a5684459e8c6c2c45f3a4184f534b50dc2
Daniel <daniel.olsson1992@gmail.com>
2019-04-22 12:10:52 +0200

    Added check for phsyical devices

--- Vulkan Engine/main.cpp
+++ Vulkan Engine/main.cpp
@@ -6,7 +6,7 @@
 #include <vector>
 #include <cstring>
 #include <cstdlib>
-#include <optinional>
+#include <optional>
 
 const int WIDTH = 800;
 const int HEIGHT = 600;
@@ -40,6 +40,14 @@ void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT
 	}
 }
 
+struct QueueFamilyIndices {
+	std::optional<uint32_t> graphicsFamily;
+
+	bool isComplete() {
+		return graphicsFamily.has_value();
+	}
+};
+
 class HelloTriangleApplication {
 public:
 	void run() {
@@ -56,6 +64,8 @@ private:
 	VkDebugUtilsMessengerEXT debugMessenger;
 
 	VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+
+
 
 	void initWindow() {
 		//Start the glfw object
@@ -101,7 +111,35 @@ private:
 
 	}
 	bool isDeviceSuitable(VkPhysicalDevice device) {
-		return true;
+		QueueFamilyIndices indices = findQueueFamilies(device);
+
+		return indices.isComplete();
+	}
+
+	QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device) {
+		QueueFamilyIndices indices;
+
+		uint32_t queueFamilyCount = 0;
+		vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+
+		std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+		vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+
+		int i = 0;
+		for (const auto& queueFamily : queueFamilies) {
+			if (queueFamily.queueCount > 0 && queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+				indices.graphicsFamily = i;
+			}
+
+			if (indices.isComplete()) {
+				break;
+			}
+
+			i++;
+		}
+		
+
+		return indices;
 	}
 
 	//Render loop
